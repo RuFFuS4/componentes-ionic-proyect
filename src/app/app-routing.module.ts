c4b3fdef8611bfcc3b1323849fd39205a2274c13
Rafa Gomez <rgr93.4@outlook.es>
2020-11-18 18:08:02 +0100

    Componentes realizados hasta el Fab

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,21 +4,45 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'inicio',
-    loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
+    loadChildren: () => import('./pages/inicio/inicio.module').then(m => m.InicioPageModule)
   },
   {
     path: 'alert',
-    loadChildren: () => import('./pages/alert/alert.module').then( m => m.AlertPageModule)
+    loadChildren: () => import('./pages/alert/alert.module').then(m => m.AlertPageModule)
   },
   {
     path: 'action-sheet',
-    loadChildren: () => import('./pages/action-sheet/action-sheet.module').then( m => m.ActionSheetPageModule)
+    loadChildren: () => import('./pages/action-sheet/action-sheet.module').then(m => m.ActionSheetPageModule)
+  },
+  {
+    path: 'avatar',
+    loadChildren: () => import('./pages/avatar/avatar.module').then(m => m.AvatarPageModule)
+  },
+  {
+    path: 'button',
+    loadChildren: () => import('./pages/button/button.module').then(m => m.ButtonPageModule)
+  },
+  {
+    path: 'card',
+    loadChildren: () => import('./pages/card/card.module').then(m => m.CardPageModule)
+  },
+  {
+    path: 'check',
+    loadChildren: () => import('./pages/check/check.module').then(m => m.CheckPageModule)
+  },
+  {
+    path: 'date-time',
+    loadChildren: () => import('./pages/date-time/date-time.module').then(m => m.DateTimePageModule)
   },
   {
     path: '',
     redirectTo: 'inicio',
     pathMatch: 'full'
   },
+  {
+    path: 'fab',
+    loadChildren: () => import('./pages/fab/fab.module').then( m => m.FabPageModule)
+  }
 ];
 
 @NgModule({
--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -20,9 +20,39 @@ export class InicioPage implements OnInit {
       redirectTo: '/action-sheet'
     },
     {
-      icon:'logo-apple-appstore',
+      icon:'alert-circle-outline',
       name: 'Alert',
       redirectTo: '/alert'
+    },
+    {
+      icon:'beaker-outline',
+      name: 'Avatar',
+      redirectTo: '/avatar'
+    },
+    {
+      icon:'radio-button-off-outline',
+      name: 'Buttons',
+      redirectTo: '/button'
+    },
+    {
+      icon:'card-outline',
+      name: 'Cards',
+      redirectTo: '/card'
+    },
+    {
+      icon:'checkmark-circle-outline',
+      name: 'Checks',
+      redirectTo: '/check'
+    },
+    {
+      icon:'calendar-outline',
+      name: 'DateTime',
+      redirectTo: '/date-time'
+    },
+    {
+      icon:'car-outline',
+      name: 'Fab',
+      redirectTo: '/fab'
     }
   ];
 
